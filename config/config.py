bab8b789213521464558e2ca4426cc4e8e3bd71d
toutpuissantged <amoussougedeon13@gmail.com>
2021-04-10 18:16:19 +0000

    init

---
+++ config/config.py
@@ -0,0 +1,12 @@
+
+
+class env():
+
+    url='http://sobricom.net/login'
+    login='username'
+    psw='password'
+    longeur_du_code=4
+    nom_fichiers_login="login_found.txt"
+    invalid_password_error_msg="invalid password"
+
+
